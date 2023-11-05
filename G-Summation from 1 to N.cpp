655e3a6986b25fe01f736e4788b8d035621b8a46
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-05 12:02:19 +0000

    G. Summation from 1 to N

---
+++ G-Summation from 1 to N.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned long long int number;
+
+    cin >> number;
+
+    cout << number * (number + 1) / 2 << endl;
+
+    return (0);
+}
