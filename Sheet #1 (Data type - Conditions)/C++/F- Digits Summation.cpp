f303e0477ca7eb85e88b94794205b2635b6785e3
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-11-06 12:36:02 +0000

    F. Digits Summation

---
+++ Sheet #1 (Data type - Conditions)/C++/F- Digits Summation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long num_1, num_2;
+
+    cin >> num_1 >> num_2;
+
+    cout << (num_1 % 10) + (num_2 % 10) << endl;
+
+    return (0);
+}
