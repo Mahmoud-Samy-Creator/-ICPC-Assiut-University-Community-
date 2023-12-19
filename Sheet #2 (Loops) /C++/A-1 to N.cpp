cbd3137a0cdee1e1c0c3a6ba8803726e7aa412c9
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-19 14:20:58 +0000

    A. 1 to N

---
+++ Sheet #2 (Loops) /C++/A-1 to N.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int limit, i;
+
+    cin >> limit;
+
+    for(i = 1 ; i <= limit ; i++)
+    {
+        cout << i << endl;
+    }
+
+    return (0);
+}
